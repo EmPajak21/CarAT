24ba7a40febf6b0a4e6236a9446b9a8de845b335
Emma Pajak <emmaevapajak@gmail.com>
2025-05-02 21:20:19 +0100

    Added Sankey feature
    Closes #2

--- carat/build/lib/processing/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Data processing components for project_name."""
-
-from .preprocessor import DataPreprocessor  # Export main function
-from .datatypes import PreProcessConfig, PostProcessConfig  # Export dataclasses
--- carat/vis/__init__.py
+++ carat/vis/__init__.py
@@ -1,3 +1,4 @@
 """Visualization components for carat."""
 
 from .vc_graph import mermaid_plot
+from .sankey import SankeyDiagramGenerator
